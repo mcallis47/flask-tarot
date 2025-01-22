f8419e4646d2b5a4dcb4291e2606b4924b5e38ae
mcallis47 <mcallis47@gmail.com>
2025-01-22 14:06:13 -0500

    Working POC for sample

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
-import requests
 
 app = Flask(__name__)
 
-EXTERNAL_SERVICE_URL= "localhost:8080"
+EXTERNAL_SERVICE_URL = "localhost:8080"
 
 @app.route('/tarot-reading', methods=['POST'])
 def tarot_reading():
     posted_data = request.get_json()
-    
+    print(posted_data.get("deck"))
+    return jsonify(posted_data.get("deck"))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
